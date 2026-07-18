54cc5eb7b1f768fe2db3b5629393c5ff0646e13d
themanoj1 <manojkumarsaw949@gmail.com>
2026-07-19 00:56:03 +0530

    first commit

---
+++ src/test/java/com/DevOps/Jenkin/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevOps.Jenkin.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
